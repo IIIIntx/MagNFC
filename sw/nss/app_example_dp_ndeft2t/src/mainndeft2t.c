36b18b2a529caac8c01aec72a3ec530acacb84f8
IMON_HKUSTGZ\87740 <2124681238@qq.com>
2024-12-06 12:32:04 +0800

    lower power consumption, add interrupt and sleep mode

--- sw/nss/app_example_dp_ndeft2t/src/mainndeft2t.c
+++ sw/nss/app_example_dp_ndeft2t/src/mainndeft2t.c
@@ -24,7 +24,6 @@
 
 /** The URL will be used in a single-record NDEF message. */
 #define MAX_URI_PAYLOAD (254 - NDEFT2T_MSG_OVERHEAD(true, NDEFT2T_URI_RECORD_OVERHEAD(true)))
-static const uint8_t sUrl[MAX_URI_PAYLOAD + 1 /* NUL */] = "nxp.com/NTAGSMARTSENSOR";
 
 /**
  * The text and the MIME data are always presented together, in a dual-record NDEF message.
@@ -41,12 +40,6 @@ static uint8_t sBytes[MAX_MIME_PAYLOAD] = {0, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE};
 
 /* ------------------------------------------------------------------------- */
 
-/**
- * Used to determine which NDEF message must be generated.
- * - @c true: generate a single-record NDEF message containing a URL.
- * - @c false: generate a dual-record NDEF message containing a TEXT and a MIME record.
- */
-static bool sState;
 
 static volatile bool sButtonPressed = false; /** @c true when the WAKEUP button is pressed on the Demo PCB */
 static volatile bool sMsgAvailable = false; /** @c true when a new NDEF message has been written by the tag reader. */
@@ -57,17 +50,14 @@ static void GenerateNdef_TextMime(int measurement[5]);
 //static void ParseNdef(void);
 
 /* ------------------------------------------------------------------------- */
-
-/**
- * Handler for PIO0_0 / WAKEUP pin.
- * Overrides the WEAK function in the startup module.
- */
-void PIO0_IRQHandler(void)
-{
-    Chip_GPIO_ClearInts(NSS_GPIO, 0, 1);
-    sButtonPressed = true; /* Handled in main loop */
+void I2D_IRQHandler(void){
+	if ((Chip_I2D_Int_GetRawStatus(NSS_I2D) & I2D_INT_CONVERSION_RDY)) {
+		if ((Chip_I2D_ReadStatus(NSS_I2D) & I2D_STATUS_CONVERSION_DONE)) {
+		/* No need to check interrupt flag here, as I2D_STATUS_CONVERSION_DONE status flag holds the same meaning. */
+		Chip_I2D_Int_ClearRawStatus(NSS_I2D, I2D_INT_CONVERSION_RDY);
+		}
+	}
 }
-
 /**
  * Called under interrupt.
  * @see NDEFT2T_FIELD_STATUS_CB
@@ -96,23 +86,6 @@ void App_MsgAvailableCb(void)
 
 /* ------------------------------------------------------------------------- */
 
-///** Generates a single-record NDEF message containing a URL, and copies it to the NFC shared memory. */
-//static void GenerateNdef_Url(void)
-//{
-//    uint8_t instance[NDEFT2T_INSTANCE_SIZE];
-//    uint8_t buffer[NFC_SHARED_MEM_BYTE_SIZE ];
-//    NDEFT2T_CREATE_RECORD_INFO_T recordInfo = {.pString = NULL /* don't care */,
-//                                               .shortRecord = true,
-//                                               .uriCode = 0x01 /* "http://www." */};
-//    NDEFT2T_CreateMessage(instance, buffer, NFC_SHARED_MEM_BYTE_SIZE, true);
-//    if (NDEFT2T_CreateUriRecord(instance, &recordInfo)) {
-//        if (NDEFT2T_WriteRecordPayload(instance, sUrl, (int)strlen((char *)sUrl))) {
-//            NDEFT2T_CommitRecord(instance);
-//        }
-//    }
-//    NDEFT2T_CommitMessage(instance); /* Copies the generated message to NFC shared memory. */
-//}
-
 /** Generates a dual-record NDEF message containing a TEXT and a MIME record, and copies it to the NFC shared memory. */
 static void GenerateNdef_TextMime(int measurement[10])
 {
@@ -137,46 +110,9 @@ static void GenerateNdef_TextMime(int measurement[10])
             NDEFT2T_CommitRecord(instance);
         }
     }
-//    if (NDEFT2T_CreateMimeRecord(instance, &mimeRecordInfo)) {
-//        if (NDEFT2T_WriteRecordPayload(instance, sBytes, sizeof(sBytes))) {
-//            NDEFT2T_CommitRecord(instance);
-//        }
-//    }
+
     NDEFT2T_CommitMessage(instance); /* Copies the generated message to NFC shared memory. */
 }
-
-///** Parses the NDEF message in the NFC shared memory, and copies the TEXT and MIME payloads. */
-//static void ParseNdef(void)
-//{
-//    uint8_t instance[NDEFT2T_INSTANCE_SIZE];
-//    uint8_t buffer[NFC_SHARED_MEM_BYTE_SIZE];
-//    NDEFT2T_PARSE_RECORD_INFO_T recordInfo;
-//    int len = 0;
-//    uint8_t *pData = NULL;
-//
-//    if (NDEFT2T_GetMessage(instance, buffer, NFC_SHARED_MEM_BYTE_SIZE)) {
-//        while (NDEFT2T_GetNextRecord(instance, &recordInfo) != false) {
-//            pData = (uint8_t *)NDEFT2T_GetRecordPayload(instance, &len);
-//            switch (recordInfo.type) {
-//                case NDEFT2T_RECORD_TYPE_TEXT:
-//                    if ((size_t)len <= MAX_TEXT_PAYLOAD) {
-//                        memcpy(sText, pData, (size_t)len);
-//                        memset(sText + len, 0, MAX_TEXT_PAYLOAD - (size_t)len);
-//                    }
-//                    break;
-//                case NDEFT2T_RECORD_TYPE_MIME:
-//                    if ((size_t)len <= MAX_MIME_PAYLOAD) {
-//                        memcpy(sBytes, pData, (size_t)len);
-//                        memset(sBytes + len, 0, MAX_MIME_PAYLOAD - (size_t)len);
-//                    }
-//                    break;
-//                default:
-//                    /* ignore */
-//                    break;
-//            }
-//        }
-//    }
-//}
 
 /* ------------------------------------------------------------------------- */
 
@@ -187,38 +123,39 @@ int main(void)
     UartTx_Init();
     int i;
     Chip_Clock_System_SetClockFreq(1 * 1000 * 1000);
-//    Chip_IOCON_Init(NSS_IOCON);
+    Chip_SysCon_Peripheral_DisablePower(SYSCON_PERIPHERAL_POWER_C2D);
+    Chip_SysCon_Peripheral_DisablePower(SYSCON_PERIPHERAL_POWER_TSEN);
 
-    Chip_IOCON_SetPinConfig(NSS_IOCON, IOCON_ANA0_0, IOCON_FUNC_1);
+
     Chip_ADCDAC_Init(NSS_ADCDAC0);
     Chip_ADCDAC_SetMuxDAC(NSS_ADCDAC0, ADCDAC_IO_ANA0_0);
     Chip_ADCDAC_SetModeDAC(NSS_ADCDAC0, ADCDAC_CONTINUOUS);
+    Chip_ADCDAC_SetModeADC(NSS_ADCDAC0, ADCDAC_SINGLE_SHOT);
+    Chip_ADCDAC_SetInputRangeADC(NSS_ADCDAC0, ADCDAC_INPUTRANGE_WIDE);
+    Chip_IOCON_SetPinConfig(NSS_IOCON, IOCON_ANA0_0, IOCON_FUNC_1);
     Chip_ADCDAC_WriteOutputDAC(NSS_ADCDAC0, 1500);
+    Chip_IOCON_SetPinConfig(NSS_IOCON, IOCON_ANA0_4, IOCON_FUNC_1); /* Set pin function to analog */
+    Chip_I2D_Init(NSS_I2D);
+	Chip_I2D_Setup(NSS_I2D, I2D_SINGLE_SHOT, I2D_SCALER_GAIN_10_1, I2D_CONVERTER_GAIN_LOW, 10);
+	Chip_I2D_SetMuxInput(NSS_I2D, I2D_INPUT_ANA0_4);
+	Chip_I2D_Int_SetEnabledMask(NSS_I2D,I2D_INT_CONVERSION_RDY);
+	NVIC_EnableIRQ(I2D_IRQn);
 
     int i2dValue;
 	int i2dNativeValue;
 	int measurement[10];
 
-
-	Chip_IOCON_SetPinConfig(NSS_IOCON, IOCON_ANA0_4, IOCON_FUNC_1); /* Set pin function to analog */
-	Chip_I2D_Init(NSS_I2D);
-	Chip_I2D_Setup(NSS_I2D, I2D_SINGLE_SHOT, I2D_SCALER_GAIN_100_1, I2D_CONVERTER_GAIN_LOW, 5);
-	Chip_I2D_SetMuxInput(NSS_I2D, I2D_INPUT_ANA0_4);
 	for (;;)
 	{
-		for(i;i<10;i++){
+		for(i=0;i<10;i++){
 			Chip_I2D_Start(NSS_I2D);
-			while (!(Chip_I2D_ReadStatus(NSS_I2D) & I2D_STATUS_CONVERSION_DONE)) {
-				; /* wait */
-			}
+			Chip_PMU_PowerMode_EnterSleep();
 			i2dNativeValue = Chip_I2D_GetValue(NSS_I2D);
-			i2dValue = Chip_I2D_NativeToPicoAmpere(i2dNativeValue, I2D_SCALER_GAIN_100_1, I2D_CONVERTER_GAIN_LOW, 5);
-
+			i2dValue = Chip_I2D_NativeToPicoAmpere(i2dNativeValue, I2D_SCALER_GAIN_10_1, I2D_CONVERTER_GAIN_LOW, 10);
 			//	 UART print to give out data
-
-			UartTx_Printf("%d\r\n", i2dValue);
 	//		UartTx_DeInit();
-			measurement[i] = i2dValue/100000;
+			measurement[i] = i2dValue/10000;
+			UartTx_Printf("%d\r\n", measurement[i]);
 		}
 		if (sFieldPresent) { /* Update the NDEF message once when there is an NFC field */
 			GenerateNdef_TextMime(measurement);
@@ -226,83 +163,6 @@ int main(void)
 			sText[0] = (uint8_t)((sText[0] == '9') ? '0' : (sText[0] + 1));
 			sBytes[0]++;
 		}
-		i = 0;
-
-
-
-
-//	while (sFieldPresent) {
-//		if (sMsgAvailable) {
-//			sMsgAvailable = false;
-//			ParseNdef();
-//		}
-//		Chip_Clock_System_BusyWait_ms(10);
-//	}
 	}
 
-//    Chip_IOCON_SetPinConfig(NSS_IOCON, IOCON_ANA0_0, IOCON_FUNC_1);
-//    Chip_ADCDAC_Init(NSS_ADCDAC0);
-//	Chip_ADCDAC_SetMuxADC(NSS_ADCDAC0, ADCDAC_IO_ANA0_0);
-//	Chip_ADCDAC_SetInputRangeADC(NSS_ADCDAC0, ADCDAC_INPUTRANGE_WIDE);
-//	Chip_ADCDAC_SetModeADC(NSS_ADCDAC0, ADCDAC_SINGLE_SHOT);
-//	int adcInput=99;
-//
-//    NVIC_EnableIRQ(PIO0_IRQn); /* PIO0_IRQHandler is called when this interrupt fires. */
-//    Chip_GPIO_EnableInt(NSS_GPIO, 0, 1);
-//
-//
-//    for (;;) {
-//    	int vMeasure;
-//		// Toggle LED
-//		LED_Toggle(LED_RED);
-//
-////		// Read out ADC
-////		Chip_ADCDAC_StartADC(NSS_ADCDAC0);
-////		while (!(Chip_ADCDAC_ReadStatus(NSS_ADCDAC0) & ADCDAC_STATUS_ADC_DONE)) { /* wait */ }
-////		adcInput = Chip_ADCDAC_GetValueADC(NSS_ADCDAC0);
-////		vMeasure = ((float)adcInput*1.6)/4096;
-//
-//        if (sButtonPressed) {
-//            sButtonPressed = false;
-//            sState = !sState; /* Switch between generating URL and TEXT+MIME. */
-//        }
-//        if (sFieldPresent) { /* Update the NDEF message once when there is an NFC field */
-//            if (sState) {
-//                GenerateNdef_Url();
-//            }
-//            else {
-//                GenerateNdef_TextMime(vMeasure);
-//                /* Update the payloads for the next message. */
-//                sText[0] = (uint8_t)((sText[0] == '9') ? '0' : (sText[0] + 1));
-//                sBytes[0]++;
-//            }
-//        }
-//        while (sFieldPresent) {
-//            if (sMsgAvailable) {
-//                sMsgAvailable = false;
-//                ParseNdef();
-//            }
-//            Chip_Clock_System_BusyWait_ms(10);
-//        }
-//    }
-//
-////    while (1) {
-////            // Toggle LED
-////            LED_Toggle(LED_RED);
-////
-////            // Read out ADC
-////            Chip_ADCDAC_StartADC(NSS_ADCDAC0);
-////            while (!(Chip_ADCDAC_ReadStatus(NSS_ADCDAC0) & ADCDAC_STATUS_ADC_DONE)) { /* wait */ }
-////            adcInput = Chip_ADCDAC_GetValueADC(NSS_ADCDAC0);
-////
-////            // UART print to give out data
-////            UartTx_Init();
-////            UartTx_Printf("ADC:%d\n", adcInput);
-////            UartTx_DeInit();
-////
-////            // Pause
-////            Chip_Clock_System_BusyWait_ms(250);
-////        }
-//
-//    return 0;
 }
